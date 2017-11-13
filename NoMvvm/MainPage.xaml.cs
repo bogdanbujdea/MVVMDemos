306bf5fae0dfc5baae0f40bd7defdc2eb5b53f38
Bogdan Bujdea <bogdan@thewindev.net>
2017-11-14 00:44:17 +0200

    No MVVM example

---
+++ NoMvvm/MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Windows.UI.Popups;
+using Windows.UI.Xaml;
+
+namespace NoMvvm
+{
+    public sealed partial class MainPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void SignupButtonClicked(object sender, RoutedEventArgs e)
+        {
+           await new MessageDialog($"Your info is: username({Username.Text}), email({Email.Text}), password({Password.Text}).").ShowAsync();
+
+        }
+    }
+}
